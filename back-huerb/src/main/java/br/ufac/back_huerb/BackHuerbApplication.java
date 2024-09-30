30f7d4bb3fb43d49bf6b5ceb4e77976124e6b02c
Gardiny <igor20091983@gmail.com>
2024-09-30 18:51:01 -0300

    Back and front > #01

---
+++ back-huerb/src/main/java/br/ufac/back_huerb/BackHuerbApplication.java
@@ -0,0 +1,13 @@
+package br.ufac.back_huerb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackHuerbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackHuerbApplication.class, args);
+	}
+
+}
