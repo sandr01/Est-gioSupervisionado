71c091b52015574f3a0cb88189b44f6456581624
sandr01 <alessandropb.fj@gmail.com>
2024-10-08 23:48:54 -0500

    model view

--- back-huerb/src/main/java/br/ufac/back_huerb/BackHuerbApplication.java
+++ back-huerb/src/main/java/br/ufac/back_huerb/BackHuerbApplication.java
@@ -8,7 +8,7 @@ public class BackHuerbApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackHuerbApplication.class, args);
+		System.out.println("aloooo");
 	}
 
-	
 }
--- front-huerb/src/app/app.routes.ts
+++ front-huerb/src/app/app.routes.ts
@@ -1,3 +1,26 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AluguelComponent } from './aluguel/aluguel.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EstoqueComponent } from './estoque/estoque.component';
+import { HomeComponent } from './home/home.component';
+import { RelatorioComponent } from './relatorio/relatorio.component';
+import { SolicitacoesComponent } from './solicitacoes/solicitacoes.component';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+  { path: '', component: LoginComponent }, // Página inicial
+  { path: 'aluguel', component: AluguelComponent },
+  { path: 'cadastro', component: CadastroComponent },
+  { path: 'estoque', component: EstoqueComponent },
+  { path :'home', component: HomeComponent},
+  { path: 'relatorio', component: RelatorioComponent },
+  { path: 'solicitacoes', component: SolicitacoesComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
